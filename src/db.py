1d5ac33b6b5cc0aee09d5ab80eaad98856aac476
uditgulati <uditgulati0@gmail.com>
2020-02-16 13:17:23 +0530

    complete db.matches

--- src/db.py
+++ src/db.py
@@ -1,7 +1,7 @@
 # Database API
 
 import os
-import sqlite
+import sqlite3
 
 
 
@@ -9,27 +9,46 @@ class DB():
   '''
     Database handler class
   '''
-  def __init__(self, hostname, username, password, database):
-    # init db connection
-    self.database = database
-    try:
-      self.connection = mysql.connect(
-        hostname, username, password, 
-        database, cursorclass=cursors.DictCursor)  
 
-      self.connection.autocommit(False)
+
+  def __init__(self, db_file):
+    # init db connection
+    self.db_file = db_file
+    try:
+      self.connection = sqlite3.connect(db_file)  
+
+      # self.connection.autocommit(False)
       self.cursor = self.connection.cursor()
 
     except e:
-      print("Connection error: unable to connect to db")
+      print("DB error: unable to connect to db")
 
-  def insert_fingerprints(self, fingerprints):
-    # insert all fingerprints into db
-    pass
+
+  def insert_fingerprint(self, sha, song_id, offset):
+    # insert fingerprint into db
+    # insert (sha, song_id, offset) tuple
+    try:  
+      self.connection.execute('''''')
+      self.connection.commit()
+    except e:
+      print("DB error: unable to insert to db")   
+
   def query(self, fingerprint):
     # find matching fingerprints in db with the given fingerprint
-    pass
-  def matches(self):
-    # combine all matches for the unknown sample and calculate their offsets
-    pass
-  
+    try:
+      self.connection.execute()
+    except e:
+      print("DB error: unable to query db")
+
+  def matches(self, fingerprints):
+    # find all fingerprints matches with their offsets
+    # for given list of fingerprints
+    matches = []
+
+    for fp in fingerprints:
+      sha, song_id, offset = fp
+      matches_for_curr_sha = self.connection.query(sha)
+      for row in matches_for_curr_sha:
+        matches.append((row[0], row[1] - offset))
+    
+    return matches
